ace33b76c7c8ce8f20be75b11d3196646f1843e3
Cooper Saye <coopersaye@gmail.com>
2020-12-04 18:56:56 -0800

    Implement door open check.

---
+++ Assets/PuzzleMansion/Objects/Door/Door.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace PuzzleMansion.Objects
+{
+    public class Door : MonoBehaviour
+    {
+        public void OpenDoor()
+        {
+            Debug.Log($"Opening door at {transform.position}");
+        }
+    }
+}
--- Assets/PuzzleMansion/Scripts/Player.cs
+++ Assets/PuzzleMansion/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using PuzzleMansion.Objects;
+using UnityEngine;
 
 namespace PuzzleMansion
 {
@@ -12,9 +13,11 @@ namespace PuzzleMansion
 
         [Header("References")]
         [SerializeField] private Rigidbody2D rb = null;
+        [SerializeField] private Transform doorPoint = null;
 
         Vector2 currentVelocity = new Vector2();
 
+        // Whether player is currently on the ground
         private bool Grounded
         {
             get { return Mathf.Abs(rb.velocity.y) < Operation.epsilon; }
@@ -24,24 +27,57 @@ namespace PuzzleMansion
         {
             Jump();
             Move();
+            Up();
         }
 
+        // Jumps
         private void Jump()
         {
+            // If jump key pressed and player grounded
             if (Input.GetKeyDown(KeyCode.Space) && Grounded)
             {
+                // Add jump force
                 rb.AddForce(new Vector2(0, jumpForce));
             }
         }
 
+        // Moves player left and right
         private void Move()
         {
+            // Get x direction from left and right keys
             int xDirection = 0;
-            if (Input.GetKey("a")) xDirection -= 1;
-            if (Input.GetKey("d")) xDirection += 1;
+            if (Input.GetKey(KeyCode.A)) xDirection -= 1;
+            if (Input.GetKey(KeyCode.D)) xDirection += 1;
+
+            // Get target velocity from x direction
             Vector2 targetVelocity = new Vector2(xDirection * movementForce, rb.velocity.y);
     
+            // Smooth damp velocity based on target velocity
             rb.velocity = Vector2.SmoothDamp(rb.velocity, targetVelocity, ref currentVelocity, smoothTime, maxSpeed);
+        }
+
+        // Checks door entry on up key press
+        private void Up()
+        {
+            // If up key pressed and player grounded
+            if (Input.GetKeyDown(KeyCode.W) && Grounded)
+            {
+                // Check for door
+                Collider2D[] results = Physics2D.OverlapPointAll(doorPoint.position);
+                foreach (Collider2D hitCol in results)
+                {
+                    // If found self, continue
+                    if (hitCol.gameObject == gameObject) continue;
+
+                    // If door component found, open door and break
+                    Door doorComponent = hitCol.gameObject.GetComponent<Door>();
+                    if (doorComponent != null)
+                    {
+                        doorComponent.OpenDoor();
+                        break;
+                    }
+                }
+            }
         }
     }
 }
