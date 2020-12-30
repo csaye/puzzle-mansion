9c26539d7bac1883787a871380072b043726d4b6
Cooper Saye <coopersaye@gmail.com>
2020-12-29 20:43:23 -0800

    Implement focus collider.

---
+++ Assets/PuzzleMansion/Scripts/FocusCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace PuzzleMansion
+{
+    public class FocusCollider : MonoBehaviour
+    {
+        [Header("Attributes")]
+        [SerializeField] private Vector2 focusPosition = new Vector2();
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            // If player, focus camera
+            if (col.CompareTag("Player")) PlayerCamera.instance.Focus(focusPosition);
+        }
+    }
+}
