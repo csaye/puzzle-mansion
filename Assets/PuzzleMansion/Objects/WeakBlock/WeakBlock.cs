477f6bfd8feb7b9a1145b1553314b4c92c8c8776
Cooper Saye <coopersaye@gmail.com>
2020-12-31 12:53:48 -0800

    Implement weak block.

---
+++ Assets/PuzzleMansion/Objects/WeakBlock/WeakBlock.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace PuzzleMansion.Objects
+{
+    public class WeakBlock : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Rigidbody2D weakBlockRb = null;
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            // If dynamic rigidbody found
+            Rigidbody2D rb = col.GetComponent<Rigidbody2D>();
+            if (rb != null && rb.bodyType == RigidbodyType2D.Dynamic)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 0); // Reset y velocity
+                weakBlockRb.bodyType = RigidbodyType2D.Dynamic; // Fall
+            }
+        }
+    }
+}
