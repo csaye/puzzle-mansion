afc0007996c2accf8fa867640b8a2d6611f4937f
Cooper Saye <coopersaye@gmail.com>
2020-12-30 17:52:09 -0800

    Remove focus collider.

--- Assets/PuzzleMansion/Scripts/PlayerCamera.cs
+++ Assets/PuzzleMansion/Scripts/PlayerCamera.cs
@@ -5,64 +5,51 @@ namespace PuzzleMansion
     public class PlayerCamera : MonoBehaviour
     {
         [Header("Attributes")]
-        [SerializeField] private float lerpTime = 0;
-        // [SerializeField] private bool restrictView = false;
-        // [SerializeField] private Vector2 minView = new Vector2();
-        // [SerializeField] private Vector2 maxView = new Vector2();
+        [SerializeField] private bool restrictView = false;
+        [SerializeField] private Vector2 minView = new Vector2();
+        [SerializeField] private Vector2 maxView = new Vector2();
 
-        // [Header("References")]
-        // [SerializeField] private Camera playerCamera = null;
-        // [SerializeField] private Transform cameraFocusTransform = null;
+        [Header("References")]
+        [SerializeField] private Camera playerCamera = null;
+        [SerializeField] private Transform cameraFocusTransform = null;
 
-        // private void Update()
-        // {
-        //     Move();
-        // }
-
-        // private Coroutine co;
-        // private int tweenId;
-
-        public static PlayerCamera instance;
-
-        private void Awake()
+        private void Update()
         {
-            instance = this;
+            Move();
         }
 
-        public void Focus(Vector2 focusPosition)
+        private void Move()
         {
-            LeanTween.move(gameObject, focusPosition, lerpTime);
+            // Get focus position
+            Vector2 focusPosition = cameraFocusTransform.position;
+
+            // If not restricting view, set position and return
+            if (!restrictView)
+            {
+                transform.position = new Vector3(focusPosition.x, focusPosition.y, transform.position.z);
+                return;
+            }
+
+            // Get minimum and maximum world points on screen
+            Vector2 screenMin = playerCamera.ScreenToWorldPoint(new Vector2(0, 0));
+            Vector2 screenMax = playerCamera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+            // Get minimum and maximum camera positions
+            float minX = transform.position.x + (minView.x - screenMin.x);
+            float maxX = transform.position.x + (maxView.x - screenMax.x);
+            float minY = transform.position.y + (minView.y - screenMin.y);
+            float maxY = transform.position.y + (maxView.y - screenMax.y);
+
+            // Clamp camera view to be within min and max range
+            float x = Mathf.Clamp(focusPosition.x, minX, maxX);
+            float y = Mathf.Clamp(focusPosition.y, minY, maxY);
+
+            // If camera out of bounds, set to center
+            if (screenMax.x - screenMin.x > maxView.x - minView.x) x = 0;
+            if (screenMax.y - screenMin.y > maxView.y - minView.y) y = 0;
+
+            // Move camera to position
+            transform.position = new Vector3(x, y, transform.position.z);
         }
-
-        // private void Move()
-        // {
-        //     // Get focus position
-        //     Vector2 focusPosition = cameraFocusTransform.position;
-
-        //     // If not restricting view, set position and return
-        //     if (!restrictView)
-        //     {
-        //         transform.position = new Vector3(focusPosition.x, focusPosition.y, transform.position.z);
-        //         return;
-        //     }
-
-        //     // Get minimum and maximum world points on screen
-        //     Vector2 screenMin = playerCamera.ScreenToWorldPoint(new Vector2(0, 0));
-        //     Vector2 screenMax = playerCamera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
-
-        //     // Get minimum and maximum camera positions
-        //     float minX = transform.position.x + (minView.x - screenMin.x);
-        //     float maxX = transform.position.x + (maxView.x - screenMax.x);
-        //     float minY = transform.position.y + (minView.y - screenMin.y);
-        //     float maxY = transform.position.y + (maxView.y - screenMax.y);
-
-        //     // Clamp camera view to be within min and max range
-        //     float x, y;
-        //     if (minX > maxX) x = focusPosition.x; else x = Mathf.Clamp(focusPosition.x, minX, maxX);
-        //     if (minY > maxY) y = focusPosition.y; else y = Mathf.Clamp(focusPosition.y, minY, maxY);
-
-        //     // Move camera to position
-        //     transform.position = new Vector3(x, y, transform.position.z);
-        // }
     }
 }
