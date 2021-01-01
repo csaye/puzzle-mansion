a382752017d679db19257a8ce2da5f66f856252e
Cooper Saye <coopersaye@gmail.com>
2020-12-31 23:55:19 -0800

    Update moving platform.

--- Assets/PuzzleMansion/Objects/MovingPlatform/MovingPlatform.cs
+++ Assets/PuzzleMansion/Objects/MovingPlatform/MovingPlatform.cs
@@ -6,15 +6,23 @@ namespace PuzzleMansion.Objects
     public class MovingPlatform : MonoBehaviour
     {
         [Header("Attributes")]
+        [SerializeField] private bool loop = false;
         [SerializeField] private Vector2[] positions = null;
         [SerializeField] [Range(1, 10)] private float speed = 1;
 
         [Header("References")]
         [SerializeField] private Rigidbody2D rb = null;
 
-        private void Start()
+        private bool started = false;
+
+        private void OnTriggerEnter2D(Collider2D col)
         {
-            StartCoroutine(Move(0));
+            // If grounded player
+            if (!started && col.CompareTag("Player") && Player.instance.grounded)
+            {
+                started = true;
+                StartCoroutine(Move(0));
+            }
         }
 
         private IEnumerator Move(int positionIndex)
@@ -34,10 +42,11 @@ namespace PuzzleMansion.Objects
             // Set velocity and wait
             rb.velocity = velocity;
             yield return new WaitForSeconds(time);
+            rb.velocity = Vector2.zero;
 
             // Get next position index and move
             int nextPositionIndex = positionIndex == positions.Length - 1 ? 0 : positionIndex + 1;
-            StartCoroutine(Move(nextPositionIndex));
+            if (loop || nextPositionIndex != 0) StartCoroutine(Move(nextPositionIndex));
         }
     }
 }
